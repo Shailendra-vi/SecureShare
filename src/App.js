487ccf612c1683e0d1d22463f870528cd8255ccc
Shailendra Vishwakarma <shailendrasv0110@gmail.com>
2022-10-29 22:19:06 +0530

    working-frontent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Button from '@material-ui/core/Button';
 import './first.css';
 
 import UploadFiles from "./components/upload-file-component";
@@ -7,18 +6,10 @@ import UploadFiles from "./components/upload-file-component";
 const App = () => {
   return (
     <>
-    {/* <div className="button">
-      <input type="file" style={{display: "none"}} id="contained-button-file" />
-      <label htmlFor="contained-button-file" >
-        <Button variant="contained" color="secondary" component="span">
-          Upload
-        </Button>
-      </label>
-    </div> */}
-    <div className="container" style={{ width: "600px" }}>
-      <div style={{ margin: "20px" }}>
-        <h3>SecureShare.com</h3>
-        <h4>React upload Files</h4>
+    <div className="container" style={{ width: "600px", margin: "20px"}}>
+      <div>
+        <h1>SecureShare.com</h1>
+        <h4>Upload Your Files</h4>
       </div>
       <UploadFiles />
     </div>
--- src/components/upload-file-component.js
+++ src/components/upload-file-component.js
@@ -1,88 +1,110 @@
 import React, { Component } from "react";
 import axios from "axios";
-// import Button from '@material-ui/core/Button';
+
+const options=[
+  {
+    label:"Encrypt",
+    value:"enc"
+  },
+  {
+    label:"Decrypt",
+    value:"dec"
+  }
+]
 
 export default class UploadFiles extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        currentFileName:"",
         currentFile: undefined,
-        password: "shailendra",
-        mode:"enc",
-        progress: 0,
-        message: "",
-  
-        // fileInfos: [],
+        password: "thisismypassword",
+        mode:"dec",
+        response: "",
+        isUploaded: false,
       };
       this.selectFile=this.selectFile.bind(this);
       this.upload=this.upload.bind(this);
+      this.download=this.download.bind(this);
+      this.setMode=this.setMode.bind(this);
+      this.setPassword=this.setPassword.bind(this);
   }
   selectFile(event) {
     this.setState({
       currentFile: event.target.files[0],
+      currentFileName: event.target.files[0]["name"],
     });
-    console.log(event.target.files[0]);
+    console.log(event.target.files[0]["name"]);
+  }
+  setPassword(e){
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  setMode(e){
+    this.setState({
+      mode: e.target.value,
+    })
+  }
+  download(){
+    axios({
+      url: "http://localhost:9090/download/"+this.state.currentFileName,
+      method: 'GET',
+      responseType: 'blob',
+  }).then((response) => {
+      const href = URL.createObjectURL(response.data);
+      const link = document.createElement('a');
+      link.href = href;
+      link.setAttribute('download', this.state.currentFileName);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(href);
+  });
   }
   upload(event) {
-    // console.log(this.state.currentFile);
     event.preventDefault();
     let formData = new FormData();
     formData.append("file", this.state.currentFile);
     formData.append("password", this.state.password);
     formData.append("mode", this.state.mode);
-
     axios.post("http://localhost:9090/upload-file", 
       formData
-    ).then((response)=>{
-      console.log(response);
+    )
+    .then((response)=>{
+      this.setState({
+        response: response,
+        isUploaded: true,
+      });
     });
     this.setState({
       currentFile: undefined,
     });
   }
-  
-
   render() {
-    const {
-      currentFile,
-      progress,
-      message,
-    } = this.state;
-
+    const uploaded="Pls upload file then download";
     return (
       <div>
-        {currentFile && (
-          <div className="progress">
-            <div
-              className="progress-bar progress-bar-info progress-bar-striped"
-              role="progressbar"
-              aria-valuenow={progress}
-              aria-valuemin="0"
-              aria-valuemax="100"
-              style={{ width: progress + "%" }}
-            >
-              {progress}%
-            </div>
-          </div>
-        )}
-
-    {/* <input type="file" onChange={this.selectFile} style={{display: "none"}} id="contained-button-file" />
-      <label htmlFor="contained-button-file" >
-        <Button variant="contained" color="secondary" component="span">
-          Upload
-        </Button>
-      </label> */}
-
       <label className="btn btn-default">
           <input type="file" onChange={this.selectFile} />
         </label>
+        <input type="text" name="password" onSubmit={this.setPassword} />
+        <div className="select-container">
+          <select value={this.state.mode} onChange={this.setMode}>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </div>
         {/* {this.state.currentFile} */}
-        <button className="btn btn-success" onClick={this.upload} >
+        <button className="btn btn-success" onClick={this.upload}>
           Upload
         </button>
-
+            <button className="btn btn-success" onClick={this.download}>
+              Download
+            </button>
         <div className="alert alert-light" role="alert">
-          {message}
+          {uploaded}
         </div>
       </div>
     );
